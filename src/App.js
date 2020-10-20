38db5b9db6bf909420f55210d3bc998057559766
Theelipan prabakar <theelipan@gmail.com>
2020-10-19 22:21:32 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -17,12 +18,19 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
   return (
     <div className='app'>
+      <h1>Todo List 🔰</h1>
       <div className='todo-list'>
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
